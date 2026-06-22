6b5d0afee090763cf31efabcb330708e979557d5
TNieAccStudy <tnieonlyu1706@gmail.com>
2026-06-22 17:16:35 +0700

    feat: add RecordLoggingConfig ScriptableObject

---
+++ Runtime/Utils/RecordLogging/RecordLoggingConfig.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace TnieYuPackage.Utils
+{
+    [CreateAssetMenu(fileName = "RecordLoggingConfig", menuName = "TnieYu/Record Logging Config")]
+    public class RecordLoggingConfig : ScriptableObject
+    {
+        public bool isEnabled = true;
+        public string fileName = "log.txt";
+        public int queueSize = 1000;
+        public bool appendMode = true;
+        public bool snapshotMode = false;
+        public LogTypeFlags logFilter = LogTypeFlags.Log | LogTypeFlags.Warning | LogTypeFlags.Error | LogTypeFlags.Exception | LogTypeFlags.Assert;
+
+        private static RecordLoggingConfig _instance;
+        public static RecordLoggingConfig Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = Resources.Load<RecordLoggingConfig>("RecordLoggingConfig");
+                return _instance;
+            }
+        }
+    }
+
+    [System.Flags]
+    public enum LogTypeFlags
+    {
+        Log = 1 << 0,
+        Warning = 1 << 1,
+        Error = 1 << 2,
+        Exception = 1 << 3,
+        Assert = 1 << 4,
+    }
+}
